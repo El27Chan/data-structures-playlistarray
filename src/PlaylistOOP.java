98daff71cc04cb57a6e5f4a84a060d79efaa3015
El27Chan <sulaiha@binus.ac.id>
2026-09-18 20:53:10 +0700

    Update PlaylistOOP.java
    update

--- src/PlaylistOOP.java
+++ src/PlaylistOOP.java
@@ -10,134 +10,142 @@
 import java.util.Scanner;
 
 public class PlaylistOOP {
+	
+	//Menjalankan program dan menampilkan menu sesuai peran pengguna
+	// Jika Admin bisa menambahkan lagu
+	// jika user bisa melihat list lagu
 
-    // Menjalankan program dan menampilkan menu sesuai peran pengguna
-    // Jika Admin bisa menambahkan lagu
-    // jika user bisa melihat list lagu
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); // Membuat Scanner untuk membaca input dari keyboard agar bisa diketik dan interaktif
-        Playlist playlist = new Playlist(5); //Membuat object Playlist dengan kapasitas awal 5 lagu
-        Admin admin = new Admin("Admin Playlist"); // Membuat object Admin dengan nama "Admin Playlist"
-        Member member = new Member("Pengguna Playlist");  // Membuat object Member dengan nama "Pengguna Playlist"
+
+    	Scanner input = new Scanner(System.in); // Membuat Scanner untuk membaca input dari keyboard agar bisa diketik dan interaktif
+
+    	Playlist playlist = new Playlist(10); //Membuat object Playlist dengan kapasitas awal 10 lagu
+
+    	// Data lagu awal
+        // Ditambahkan tanpa menampilkan pesan ke layar
+        playlist.tambahLaguAwal(
+            new Lagu("Perfect", "Ed Sheeran", 4.23)
+        );
+
+        playlist.tambahLaguAwal(
+            new Lagu("Shivers", "Ed Sheeran", 3.50)
+        );
 
         int pilihan;
+
         do {
-            System.out.println("\n========================================");
-            System.out.println("       PROGRAM PLAYLIST MUSIK OOP");
-            System.out.println("========================================");
-            System.out.println("1. Masuk sebagai Admin");
-            System.out.println("2. Masuk sebagai Member");
-            System.out.println("0. Keluar");
+            
+            // MENU UTAMA
+            System.out.println("\n=== MENU PLAYLIST MUSIK ===");
+            System.out.println("1. Tampilkan semua lagu");
+            System.out.println("2. Tambah lagu baru");
+            System.out.println("3. Hapus lagu berdasarkan judul");
+            System.out.println("4. Cari lagu berdasarkan judul");
+            System.out.println("5. Urutkan berdasarkan durasi");
+            System.out.println("6. Keluar");
             System.out.print("Pilih menu: ");
+
             pilihan = bacaAngka(input);
 
-            if (pilihan == 1) {
-                menuAdmin(input, admin, playlist);
-            } else if (pilihan == 2) {
-                menuMember(input, member, playlist);
-            } else if (pilihan != 0) { // kalo user memasukkan angka selain 0, 1, atau 2, muncul pilihan tidak tersedia
-                System.out.println("Pilihan tidak tersedia.");
-            }
-        } while (pilihan != 0);
+            switch (pilihan) {
 
-        System.out.println("Program selesai.");
+                // TRAVERSAL
+                case 1:
+                    playlist.tampilkanSemuaLagu();
+                    break;
+
+                // INSERTION
+                case 2:
+
+                    System.out.print("Masukkan judul lagu : ");
+                    String judul = input.nextLine();
+
+                    System.out.print("Masukkan artis      : ");
+                    String artis = input.nextLine();
+
+                    System.out.print("Masukkan durasi (menit): ");
+                    double durasi = bacaDesimal(input);
+
+                    Lagu laguBaru = new Lagu(judul, artis, durasi);
+
+                    playlist.tambahLagu(laguBaru);
+
+                    break;
+
+                // MENU 3
+                case 3:
+                    System.out.println(
+                        "Fitur hapus lagu menunggu anggota 2."
+                    );
+                    break;
+
+                // MENU 4
+                case 4:
+                    System.out.println(
+                        "Fitur cari lagu menunggu anggota 2."
+                    );
+                    break;
+
+                // MENU 5
+                case 5:
+                    System.out.println(
+                        "Fitur pengurutan lagu menunggu anggota 2."
+                    );
+                    break;
+
+                // KELUAR
+                case 6:
+                    System.out.println("Program selesai.");
+                    break;
+
+                default:
+                    System.out.println(
+                        "Pilihan tidak tersedia. Silakan pilih 1-6."
+                    );
+            }
+
+        } while (pilihan != 6);
+
         input.close();
     }
 
-    // Menampilkan menu Admin untuk menambah dan melihat daftar lagu.
-    // Method ini menampilkan seluruh menu yang bisa digunakan Admin
-    private static void menuAdmin(Scanner input, Admin admin, Playlist playlist) {
-        int pilihan; // // Variabel untuk menyimpan pilihan menu Admin
-        // Menu Admin akan terus diulang sampai user memilih 0
-        do {
-            System.out.println("\n=== MENU ADMIN ===");
-            // Memanggil method dari object Admin
-            // Digunakan untuk menampilkan hak akses Admin
-            admin.tampilkanAkses();
-            System.out.println("1. Tambah lagu");
-            System.out.println("2. Lihat daftar lagu");
-            System.out.println("0. Kembali");
-            System.out.print("Pilih menu: ");
-            pilihan = bacaAngka(input);
-
-            // Jika Admin memilih menu 1
-            // admin harus input judul lagu, nama artis dan durasinya berapa menit
-            if (pilihan == 1) {
-                System.out.print("Judul lagu  : ");
-                String judul = input.nextLine();
-                System.out.print("Nama artis  : ");
-                String artis = input.nextLine();
-                System.out.print("Durasi (menit): ");
-                // Membaca durasi menggunakan method bacaDesimal(), menggunakan double karena durasi bisa berupa angka desimal
-                double durasi = bacaDesimal(input);
-
-                // Admin menambahkan lagu baru ke dalam playlist
-                admin.tambahLagu(playlist, new Lagu(judul, artis, durasi));
-            } else if (pilihan == 2) {
-                admin.lihatPlaylist(playlist);
-            } else if (pilihan != 0) {
-                System.out.println("Pilihan tidak tersedia.");
-            }
-        } while (pilihan != 0);
-    }
-
-    // Menampilkan menu Member untuk melihat, mencari, dan menghitung durasi lagu.
-    // member bisa melihat playlist, mencari lagu, dan menghitung rata-rata durasi tetapi tidak untuk input lagu baru
-    private static void menuMember(Scanner input, Member member, Playlist playlist) {
-        int pilihan;   // Variabel untuk menyimpan pilihan menu Member
-        do {
-            System.out.println("\n=== MENU MEMBER ===");
-            member.tampilkanAkses();
-            System.out.println("1. Lihat daftar dan detail lagu");
-            System.out.println("2. Cari lagu berdasarkan judul");
-            System.out.println("3. Hitung rata-rata durasi");
-            System.out.println("0. Kembali");
-            System.out.print("Pilih menu: ");
-            pilihan = bacaAngka(input);
-
-            if (pilihan == 1) {
-                member.lihatPlaylist(playlist);
-            } else if (pilihan == 2) {
-                System.out.print("Masukkan judul yang dicari: ");
-                String kataKunci = input.nextLine();
-                member.cariLagu(playlist, kataKunci);
-            } else if (pilihan == 3) {
-                member.hitungRataRataDurasi(playlist);
-            } else if (pilihan != 0) {
-                System.out.println("Pilihan tidak tersedia.");
-            }
-        } while (pilihan != 0);
-    }
-
-    // Membaca bilangan bulat dan mencegah program berhenti saat input salah.
-    // tujuannya supaya program tidak berhenti/error kalau user mengetik huruf
+    // Membaca input angka
     private static int bacaAngka(Scanner input) {
-        while (!input.hasNextInt()) {   // Selama input berikutnya bukan integer
-            System.out.print("Input harus berupa angka. Masukkan kembali: "); // Memberi tahu user bahwa input salah
-            input.nextLine();  // Membuang input yang salah misalnya si user mengetik "abc"
-            
+
+        while (!input.hasNextInt()) {
+            System.out.print(
+                "Input harus berupa angka. Masukkan kembali: "
+            );
+            input.nextLine();
         }
+
         int angka = input.nextInt();
         input.nextLine();
+
         return angka;
     }
 
-    // Membaca bilangan desimal positif untuk durasi lagu.
-    // Dipakai saat user memasukkan durasi lagu 
+    // Membaca input durasi
     private static double bacaDesimal(Scanner input) {
-        double angka;
-        while (true) { // Loop berjalan terus sampai user memasukkan nilai yang valid
-            if (input.hasNextDouble()) { // Mengecek apakah input berupa angka desimal
-                angka = input.nextDouble(); // Membaca angka desimal
+
+        while (true) {
+
+            if (input.hasNextDouble()) {
+
+                double angka = input.nextDouble();
                 input.nextLine();
+
                 if (angka > 0) {
                     return angka;
                 }
+
             } else {
                 input.nextLine();
             }
-            System.out.print("Durasi harus berupa angka positif. Masukkan kembali: "); // Pesan akan muncul jika input salah contohnya user salah mengetik : "abc", -5, atau 0
-            
+
+            System.out.print(
+                "Durasi harus berupa angka positif. Masukkan kembali: "
+            );
         }
     }
 }
